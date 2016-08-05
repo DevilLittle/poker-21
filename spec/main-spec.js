5b292db572cd06fdde8baf03efdb106f941ba225
DevilLittle <1310265832@qq.com>
2016-08-05 13:22:10 +0800

    final modifity

--- spec/main-spec.js
+++ spec/main-spec.js
@@ -1,134 +1,139 @@
-/*global describe,it,expect,spyOn*/
-let {getFormattedInput,getNormalPoints,getAllPoints,printPointA,printPointB,getCompareResult,getFinalResult} =require('../src/main.js');
+'use strict';
+let {
+    getCards,
+    convertJkqToNumberCards,
+    getPointAndCount,
+    getComparedResult,
+    printWinner
+} = require('../src/main.js');
+
 describe('poker-21', function () {
 
-    it('should format input', function () {
+    it('should get cards from input', function () {
         let input = 'A-3-J-Q-7';
-        let formattedInput = getFormattedInput(input);
-        let expected = ['A','3','J','Q','7'];
-        expect(formattedInput).toEqual(expected)
-    });
-    it('should consider J,Q,K as 10 not contain A to get normal points' , function () {
-        let formattedInput = [ '4', '2', 'Q','J','K' ];
-        let normalPoints=getNormalPoints(formattedInput);
-        let expected = [ '4', '2', 10 ,10,10];
-        expect(normalPoints).toEqual(expected)
-    });
-    it('should consider J,Q,K as 10 contain A to get normal points', function () {
-        let formattedInput = [ 'A', '2', '3', '4', 'J' ];
-        let normalPoints=getNormalPoints(formattedInput);
-        let expected = [ 'A', '2', '3', '4', 10 ];
-        expect(normalPoints).toEqual(expected)
+        let cards = getCards(input);
+        let expected = ['A', '3', 'J', 'Q', '7'];
+        expect(cards).toEqual(expected)
     });
 
-    it('should only contains "2-10" to get all points', function () {
-        let normalPoints = [ '2','5', '9' ];
-        let allPoints=getAllPoints(normalPoints);
-        let expected = 16;
-        expect(allPoints).toEqual(expected)
-    });
-    it('should only contains "J,Q,K" to get all points', function () {
-        let formattedInput = [ 'J','Q' ];
-        let normalPoints=getNormalPoints(formattedInput);
-        let allPoints=getAllPoints(normalPoints);
-        let expected = 20;
-        expect(allPoints).toEqual(expected)
-    });
-    it('should contains "2-10" and "J,Q,K" to get all points', function () {
-        let formattedInput = ['2','J','3','2'];
-        let normalPoints=getNormalPoints(formattedInput);
-        let allPoints=getAllPoints(normalPoints);
-        let expected = 17;
-        expect(allPoints).toEqual(expected)
-    });
-    it('should consider A as 1 to get all points', function () {
-        let normalPoints = [ 'A', '2', '3', '4', '10' ];
-        let allPoints=getAllPoints(normalPoints);
-        let expected = 20;
-        expect(allPoints).toEqual(expected)
-    });
-    it('should consider A as 11 to get all points', function () {
-        let normalPoints = [ 'A', '2', '3', '4' ];
-        let allPoints=getAllPoints(normalPoints);
-        let expected = 20;
-        expect(allPoints).toEqual(expected)
-    });
-    it('should contains lots of A to get all points', function () {
-        let normalPoints = [ 'A', '2', 'A','3', '4','A' ];
-        let allPoints=getAllPoints(normalPoints);
-        let expected = 12;
-        expect(allPoints).toEqual(expected)
-    });
-    it('should contains "2-10"&&"JQK"&&"A" to get all points', function () {
-        let formattedInput = [ '2', 'A','3', 'J' ];
-        let normalPoints=getNormalPoints(formattedInput);
-        let allPoints=getAllPoints(normalPoints);
-        let expected = 16;
-        expect(allPoints).toEqual(expected)
-    });
-    it('should consider pointA>21&&pointB>21 to get compare result', function () {
-        let inputA = 'A-J-3-J';
-        let inputB = '4-7-Q-5';
-        let pointA=printPointA(inputA);
-        let pointB=printPointB(inputB);
-        let compareResult=getCompareResult(pointA,pointB);
-        let expected = 'A & B tied!';
-        expect(compareResult).toEqual(expected)
-    });
-    it('should consider pointA<=21&&pointB<=21(pointA>pointB) to get compare result', function () {
-        let inputA = 'A-3-4-J';
-        let inputB = '4-2-5';
-        let pointA=printPointA(inputA);
-        let pointB=printPointB(inputB);
-        let compareResult=getCompareResult(pointA,pointB);
-        let expected = 'A win!';
-        expect(compareResult).toEqual(expected)
-    });
-    it('should consider pointA<=21&&pointB<=21(pointA<pointB) to get compare result', function () {
-        let inputA = 'A-3-J';
-        let inputB = '4-2-3-A';
-        let pointA=printPointA(inputA);
-        let pointB=printPointB(inputB);
-        let compareResult=getCompareResult(pointA,pointB);
-        let expected = 'B win!';
-        expect(compareResult).toEqual(expected)
-    });
-    it('should consider pointA<=21&&pointB<=21(pointA===pointB) to get compare result', function () {
-        let inputA = 'A-3-4-J';
-        let inputB = '4-4-J';
-        let pointA=printPointA(inputA);
-        let pointB=printPointB(inputB);
-        let compareResult=getCompareResult(pointA,pointB);
-        let expected = 'B win!';
-        expect(compareResult).toEqual(expected)
-    });
-    it('should consider pointA<=21&&pointB<=21 to get compare result', function () {
-        let inputA = 'A-2-2-3';
-        let inputB = '4-7-7';
-        let pointA=printPointA(inputA);
-        let pointB=printPointB(inputB);
-        let compareResult=getCompareResult(pointA,pointB);
-        let expected = 'B win!';
-        expect(compareResult).toEqual(expected)
+    describe('convertJkqToNumberCards', ()=> {
+        it('should convert J,K,Q to 10', function () {
+            let formattedInput = ['4', '2', 'Q', 'J', 'K'];
+            let normalPoints = convertJkqToNumberCards(formattedInput);
+            let expected = ['4', '2', '10', '10', '10'];
+            expect(normalPoints).toEqual(expected);
+        });
+
+        it('should keep A unchanged', function () {
+            let formattedInput = ['A', '2'];
+            let normalPoints = convertJkqToNumberCards(formattedInput);
+            let expected = ['A', '2'];
+            expect(normalPoints).toEqual(expected)
+        });
     });
 
-    it('should consider pointA<=21&&pointB>21 to get compare result', function () {
-        let inputA = 'A-2-2';
-        let inputB = '4-4-7-J';
-        let pointA=printPointA(inputA);
-        let pointB=printPointB(inputB);
-        let compareResult=getCompareResult(pointA,pointB);
-        let expected = 'A win!';
-        expect(compareResult).toEqual(expected)
+
+    describe('getPointAndCount', ()=> {
+        it('should get point and count for cards 2~10', function () {
+            let numberCards = ['2', '5', '10'];
+            let pointAndCount = getPointAndCount(numberCards);
+            let expected = {
+                point: 17,
+                count: 3
+            };
+            expect(pointAndCount).toEqual(expected)
+        });
+
+        it('should get point and count when considering A as 1', function () {
+            let normalPoints = ['A', '2', '3', '4', '10'];
+            let pointAndCount = getPointAndCount(normalPoints);
+            let expected = {
+                point: 20,
+                count: 5
+            };
+            expect(pointAndCount).toEqual(expected)
+        });
+
+        it('should get point and count when considering A as 11', function () {
+            let normalPoints = ['A', '2', '3', '4'];
+            let pointAndCount = getPointAndCount(normalPoints);
+            let expected = {
+                point: 20,
+                count: 4
+            };
+            expect(pointAndCount).toEqual(expected);
+        });
+
+        it('should get point and count for multiple A cards', function () {
+            let cards = ['A', '2', 'A', '3', '4', 'A'];
+            let pointAndCount = getPointAndCount(cards);
+            let expected = {
+                point: 12,
+                count: 6
+            };
+            expect(pointAndCount).toEqual(expected)
+        });
+
+    });
+
+    describe('getCompareResult', ()=> {
+        describe('at least one >21', ()=> {
+            it('both > 21', function () {
+                let aPointAndCount = {point: 22, count: 4};
+                let bPointAndCount = {point: 28, count: 4};
+                let compareResult = getComparedResult(aPointAndCount, bPointAndCount);
+                let expected = 'tied';
+                expect(compareResult).toEqual(expected)
+            });
+            it('A > 21', function () {
+                let aPointAndCount = {point: 22, count: 4};
+                let bPointAndCount = {point: 1, count: 4};
+                let compareResult = getComparedResult(aPointAndCount, bPointAndCount);
+                let expected = 'B won';
+                expect(compareResult).toEqual(expected)
+            });
+            it('B > 21', function () {
+                let aPointAndCount = {point: 1, count: 4};
+                let bPointAndCount = {point: 22, count: 4};
+                let compareResult = getComparedResult(aPointAndCount, bPointAndCount);
+                let expected = 'A won';
+                expect(compareResult).toEqual(expected)
+            });
+        });
+
+        describe('both <= 21', ()=> {
+            it('A > B', ()=> {
+                let aPointAndCount = {point: 21, count: 4};
+                let bPointAndCount = {point: 20, count: 4};
+                let compareResult = getComparedResult(aPointAndCount, bPointAndCount);
+                let expected = 'A won';
+                expect(compareResult).toEqual(expected)
+            });
+            it('A < B', ()=> {
+                let aPointAndCount = {point: 20, count: 4};
+                let bPointAndCount = {point: 21, count: 4};
+                let compareResult = getComparedResult(aPointAndCount, bPointAndCount);
+                let expected = 'B won';
+                expect(compareResult).toEqual(expected)
+            });
+            it('A == B, and count of A > count of B', ()=> {
+                let aPointAndCount = {point: 20, count: 4};
+                let bPointAndCount = {point: 20, count: 3};
+                let compareResult = getComparedResult(aPointAndCount, bPointAndCount);
+                let expected = 'B won';
+                expect(compareResult).toEqual(expected)
+            })
+        });
     });
 
     // integration testing
-    it('should get compare result from firstStep to finalStep', function () {
-        let inputA = 'A-2-3-4-J';
-        let inputB = '4-2-Q-5';
-        let result=getFinalResult(inputA,inputB);
-        let expected = 'B win!';
-        expect(result).toEqual(expected)
+    it('should print winner for two inputs', function () {
+        spyOn(console, 'log');
+        let inputA = 'A-J-3-4';
+        let inputB = 'A-J-7';
+        printWinner(inputA, inputB);
+        let expected = 'B won';
+        expect(console.log).toHaveBeenCalledWith(expected);
     });
+
 });
- 
--- src/main.js
+++ src/main.js
@@ -1,110 +1,53 @@
 let _ = require('lodash');
 
-function getFormattedInput(input) {
+function getCards(input) {
     return input.split('-');
 }
-function getNormalPoints(formattedInput) {
-    let containsJQK = _(['J','K','Q']).some(x => formattedInput.includes(x));
-    if (containsJQK) {
-        return _.chain(formattedInput).map(element=> {
-            if (['J', 'K' ,'Q'].includes(element)) {
-                return 10;
-            } else {
-                return element;
-            }
-        }).value();
-    }
-    return formattedInput;
+function convertJkqToNumberCards(formattedInput) {
+    return _(formattedInput).map(element=> {
+        let isJQK = ['J', 'K', 'Q'].includes(element);
+        return isJQK ? '10' : element;
+    }).value();
 }
+function getPointAndCount(numberCards) {
+    let point = _(numberCards).map(element=> {
+        return element === 'A' ? 1 : parseInt(element);
+    }).sum();
 
-function getAllPoints(normalPoints) {
-    let containsA =normalPoints.includes('A');
-
-    if (containsA) {
-        let initalPoints = _(normalPoints).map(element=> {
-            return element==='A' ? 1 : parseInt(element);
-        }).sum();
-
-        if (initalPoints <= 11) {
-            initalPoints += 10;
-        }
-        return initalPoints;
-    } else {
-       return _(normalPoints).map(element => parseInt(element)).sum();
+    if (point <= 11) {
+        point += 10;
+    }
+    return {
+        point,
+        count: numberCards.length
     }
 }
-function printPointA(inputA) {
-    // console.log(inputA);
-    let formattedInputA = getFormattedInput(inputA);
-    // console.log(formattedInputA);
-    let normalPointsA = getNormalPoints(formattedInputA);
-    // console.log(normalPointsA);
-    let allPoints = getAllPoints(normalPointsA);
-    // console.log(allPoints);
-    return allPoints;
-}
-function printPointB(inputB) {
-    let formattedTagB = getFormattedInput(inputB);
-    // console.log(formattedTagB);
-    let normalPointsB = getNormalPoints(formattedTagB);
-    // console.log(normalPointsB);
-    let allPoints = getAllPoints(normalPointsB);
-    return allPoints;
-    // console.log(allPoints);
+function getComparedResult(aPointAndCount, bPointAndCount) {
+    let {point:aPoint, count:aCount} = aPointAndCount;
+    let {point:bPoint, count:bCount} = bPointAndCount;
+    if (aPoint > 21 && bPoint > 21) return "tied";
+    if (aPoint > 21) return 'B won';
+    if (bPoint > 21) return 'A won';
+    if (aPoint > bPoint) return 'A won';
+    if (bPoint > aPoint) return 'B won';
+    if (aCount > bCount) return 'B won';
+    if (bCount > aCount) return 'A won';
+    return 'tied';
 }
 
-function getCompareResult(pointA, pointB) {
-    let formattedInputA=getFormattedInput(inputA);
-    let cardNumberA=formattedInputA.length;
-    let formattedInputB=getFormattedInput(inputB);
-    // console.log(formattedInputB);
-    let cardNumberB=formattedInputB.length;
-    // console.log(cardNumberB);
-    if (pointA > 21 && pointB > 21) {
-        return "A & B tied!";
-    } else if (pointA > 21 && pointB <= 21) {
-        return 'B won!';
-    } else if (pointA <= 21 && pointB > 21) {
-        return 'A win!';
-    } else if (pointA <= 21 && pointB <= 21) {
-        if (pointA === pointB) {
-            if (cardNumberA === cardNumberB) {
-                return 'A & B tied!';
-            } else {
-                if (cardNumberA < cardNumberB) {
-                    return 'A win!';
-                } else {
-                    return 'B win!';
-                }
-            }
-        } else {
-            if (pointA > pointB) {
-                return 'A win!';
-            } else {
-                return 'B win!';
-            }
-        }
-    }
-}
-function getFinalResult(inputA, inputB) {
-    let pointA = printPointA(inputA);
-    let pointB = printPointB(inputB);
-    let compareResult = getCompareResult(pointA, pointB);
-    // console.log(compareResult);
-    return compareResult;
+function printWinner(inputA, inputB) {
+    let aPointAndCount = getPointAndCount(convertJkqToNumberCards(getCards(inputA)));
+    let bPointAndCount = getPointAndCount(convertJkqToNumberCards(getCards(inputB)));
+    let result = getComparedResult(aPointAndCount, bPointAndCount);
+    console.log(result);
 }
 let inputA = 'A-2-2-3';
 let inputB = '4-7-7';
-// let inputA = '4-2-Q-5';
-// let inputB = 'A-2-3-4-A';
-
-getFinalResult(inputA, inputB);
+printWinner(inputA, inputB);
 module.exports = {
-    getFormattedInput: getFormattedInput,
-    getNormalPoints: getNormalPoints,
-    getAllPoints:getAllPoints,
-    printPointA:printPointA,
-    printPointB:printPointB,
-    getCompareResult:getCompareResult,
-    getFinalResult:getFinalResult
+    getCards,
+    convertJkqToNumberCards,
+    getPointAndCount,
+    getComparedResult,
+    printWinner
 };
